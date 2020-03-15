1c3aaaaf279307dae8a3f7444277c9e88a6df046
Guzznorlax <b07505051@ntu.edu.tw>
2020-03-15 21:25:31 +0800

    Finished prob. 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
 
 public class Main {
    public static void main(String[] args) {
-        // Write your code here
-//		   System.out.println("Hello World.");
+       // Write your code here
+
+       // Helo World
+       System.out.println("Hello World.");
+
+       // prob. 1
+       System.out.println("====== prob. 1 APlusB ======");
         int answer = APlusB.plus(10,2);
         System.out.println(answer);
+
+        // prob. 2
+        System.out.println("====== prob. 2 IsLeapYear ======");
+        IsLeapYear ily = new IsLeapYear();
+        System.out.println(ily.determine(400));
+        System.out.println(ily.determine(200));
+        System.out.println(ily.determine(256));
+        System.out.println(ily.determine(251));
     }
 
 }
