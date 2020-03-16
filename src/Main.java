22b28751856fcdff50d1f3de231e3540a430b4ff
Guzznorlax <b07505051@ntu.edu.tw>
2020-03-16 09:04:06 +0800

    Formatted IsLeapYear.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 
 public class Main {
-   public static void main(String[] args) {
-       // Write your code here
+    public static void main(String[] args) {
+        // Write your code here
 
-       // Helo World
-       System.out.println("Hello World.");
+        // Helo World
+        System.out.println("Hello World.");
 
-       // prob. 1
-       System.out.println("====== prob. 1 APlusB ======");
-        int answer = APlusB.plus(10,2);
+        // prob. 1
+        System.out.println("====== prob. 1 APlusB ======");
+        int answer = APlusB.plus(10, 2);
         System.out.println(answer);
 
         // prob. 2
